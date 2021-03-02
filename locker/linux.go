9c8dbb66ab6ea91536f272bddc657bd321558602
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2021-03-03 08:44:13 +0900

    feat: lock (#16)

--- locker/linux.go
+++ locker/linux.go
@@ -1,11 +1,14 @@
 package locker
 
+import "os/exec"
+
 
 type LinuxLockerOperator struct {
 }
 
 
 func (op LinuxLockerOperator) ExecuteLock() (err error) {
-	// TODO: Write process of lock on Linux
-	return err
+	err = exec.Command("loginctl", "lock-session").Run()
+
+	return
 }
